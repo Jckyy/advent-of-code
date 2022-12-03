60029ec44118f69d0fb1ef4708357a9cfe74ea39
Jacky Yao <jcky.yao@gmail.com>
2022-12-04 01:41:49 +1100

    Day 1 2/2 done

--- Day 1/Calorie Counting.py
+++ Day 1/Calorie Counting.py
@@ -1,17 +1,43 @@
+import heapq
+
+# Part 1
 # Find the highest total calorie count from an individual elf
+
+# Split data on '\n' and sum each segment.
+
+
+def CreateCalorieList(data):
+    sumData = []
+    sum = 0
+    for line in data:
+        # print(data[15] == "\n")
+        if line == "\n":
+            sumData.append(sum)
+            sum = 0
+            continue
+        sum += int(line)
+    return sumData
+
+
 file = open("input.txt", 'r')
 data = file.readlines()
+file.close()
+
+sumData = CreateCalorieList(data)
+
+print("Max calories: " + str(max(sumData)))
 
 
-# print(data[15] == "\n")
+# Part 2
+# How many total calories are the top 3 elves carrying?
 
-sumData = []
-sum = 0
-for line in data:
-    if line == "\n":
-        sumData.append(sum)
-        sum = 0
-        continue
-    sum += int(line)
+# Return sum of a lit
+def SumList(list):
+    sum = 0
+    for i in list:
+        sum += i
+    return sum
 
-print(max(sumData))
+
+topCalories = heapq.nlargest(3, sumData)
+print('Sum calories of top 3 elves: ' + str(SumList(topCalories)))
