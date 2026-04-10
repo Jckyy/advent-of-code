8e032f1493cc02991e18b0411457bdd34aef29da
Jacky Yao <jcky.yao@gmail.com>
2026-04-10 12:23:35 +1000

    2024 day-01 add optimised code

--- 2024/day-01/main.py
+++ 2024/day-01/main.py
@@ -1,13 +1,19 @@
+from collections import Counter
+import time
+
+
 def read_data(file_path):
     with open(file_path, "r") as f:
         data = f.read().splitlines()
     return data
 
 
-# data = read_data("sample-input.txt")
-data = read_data("input.txt")
+use_sample = True
+file_path = "sample-input.txt" if use_sample else "input.txt"
+data = read_data(file_path)
 
 ### Part 1 ###
+start_time_1 = time.perf_counter()
 left_arr = []
 right_arr = []
 
@@ -21,30 +27,55 @@ right_arr.sort()
 
 part_1_answer = 0
 
-for i, _ in enumerate(left_arr):
-    similarity_score = abs(left_arr[i] - right_arr[i])
-    # print(similarity_score)
-    part_1_answer += similarity_score
+# for i, _ in enumerate(left_arr):
+#     similarity_score = abs(left_arr[i] - right_arr[i])
+#     # print(similarity_score)
+#     part_1_answer += similarity_score
 
+for left, right in zip(left_arr, right_arr):
+    diff = abs(left - right)
+    part_1_answer += diff
+
+end_time_1 = time.perf_counter()
+execution_time_2 = end_time_1 - start_time_1
 print(f"Part 1 answer = {part_1_answer}")
 # Submission 1: 1341714 Correct
+print(f"Part 2 execution time = {execution_time_2:.6f} seconds")
 
 
 ### Part 2 ###
-occurrences = {}
+start_time_2 = time.perf_counter()
+
+# occurrences = {}
+# part_2_answer = 0
+#
+# for left in left_arr:
+#     if left not in occurrences:
+#         occurrences[left] = 0
+#
+#         for right in right_arr:
+#             if left == right:
+#                 occurrences[left] += 1
+#
+#     similarity_score = left * occurrences[left]
+#     # print(f"similarity_score for {left} = {similarity_score}")
+#     part_2_answer += similarity_score
+#
+# print(f"part_2_answer = {part_2_answer}")
+# # Submission 1: 27384707 Correct
+
+
+### Part 2 Optimised ###
+count = Counter(right_arr)
+
 part_2_answer = 0
 
 for left in left_arr:
-    if left not in occurrences:
-        occurrences[left] = 0
+    if left in count:
+        similarity_score = left * count[left]
+        part_2_answer += similarity_score
 
-        for right in right_arr:
-            if left == right:
-                occurrences[left] += 1
-
-    similarity_score = left * occurrences[left]
-    # print(f"similarity_score for {left} = {similarity_score}")
-    part_2_answer += similarity_score
-
-print(f"part_2_answer = {part_2_answer}")
-# Submission 1: 27384707 Correct
+end_time_2 = time.perf_counter()
+execution_time_2 = end_time_2 - start_time_2
+print(f"Part 2 answer = {part_2_answer}")
+print(f"Part 2 execution time = {execution_time_2:.6f} seconds")
